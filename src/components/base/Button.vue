7575f90113f5a5e79685f81afdfc0ef599101786
kczapko <kczapko@gmail.com>
2021-11-15 21:05:44 +0100

    base button

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -1,11 +1,29 @@
 <template>
-  <button class="btn">
-    <slot></slot>
+  <button class="btn" :class="cssClass">
+    <span v-if="icon" class="material-icons btn__icon">{{ icon }}</span>
+    <span><slot></slot></span>
   </button>
 </template>
 
 <script>
 export default {
   name: 'Button',
+  props: {
+    color: {
+      type: String,
+      validator(val) {
+        return ['default', 'primary', 'danger'].includes(val);
+      },
+      default: 'default',
+    },
+    icon: String,
+  },
+  computed: {
+    cssClass() {
+      return {
+        [`btn--${this.color}`]: true,
+      };
+    },
+  },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">Home page content</div>
+  <app-button>Login</app-button>
+  <app-button color="primary">Login</app-button>
+  <app-button color="danger">Login</app-button>
+
+  <hr />
+  <app-button icon="face">Login</app-button>
+  <app-button icon="face" color="primary">Login</app-button>
+  <app-button icon="face" color="danger">Login</app-button>
 </template>
 
 <script>
 import { useMeta } from 'vue-meta';
+import AppButton from '@/components/base/Button.vue';
 
 export default {
   name: 'Home',
+  components: {
+    AppButton,
+  },
   setup() {
     useMeta({
       title: 'Home',
