631deb2d5a4653897cff196dea0bc30b8da31789
kczapko <kczapko@gmail.com>
2021-12-06 15:08:39 +0100

    unsplash base styles

--- src/components/base/BaseHeader.vue
+++ src/components/base/BaseHeader.vue
@@ -1,40 +1,39 @@
 <template lang="pug">
 header.header
-  .header__left
-    .header__logo
-      slot(name="logo")
-        router-link(:to="{ name: 'dashboard' }")
-          svg-logo-dev-challenges
-  .header__right
-    .header__user(:class="{'header__user--open' : dropdownOpen}" @click.stop)
-      .header__user-info(@click="dropdownOpen = !dropdownOpen")
-        base-user-image.header__user-image(:user="user")
-        .header__user-name.font-700 {{ username }}
-        .header__user-dropdown-arrow
-      transition(name="fade")
-        .header__user-dropdown(v-show="dropdownOpen")
-          ul.header__user-dropdown-list
-            li.header__user-dropdown-listitem
-              base-button.header__user-dropdown-link(
-                tag="router-link"
-                :to="{ name: 'dashboard' }"
-                @click="hideDropdown"
-                icon="home"
-                variant="link"
-                size="small") Home
-            li.header__user-dropdown-listitem
-              base-button.header__user-dropdown-link(
-                tag="router-link"
-                :to="{ name: 'profile' }"
-                @click="hideDropdown"
-                icon="account_circle"
-                variant="link"
-                size="small") My Profile
-            li.header__user-dropdown-listitem
-              base-button.header__user-dropdown-link(icon="group" variant="link" size="small" disabled) Group Chat
-            li.header__user-dropdown-listitem.header__user-dropdown-listitem--line
-            li.header__user-dropdown-listitem
-              base-button.header__user-dropdown-link(@click="logoutUser" icon="logout" color="danger" variant="link" size="small") Logout
+  .header__logo
+    slot(name="logo")
+      router-link(:to="{ name: 'dashboard' }")
+        svg-logo-dev-challenges
+  .header__user(:class="{'header__user--open' : dropdownOpen}" @click.stop)
+    .header__user-info(@click="dropdownOpen = !dropdownOpen")
+      base-user-image.header__user-image(:user="user")
+      .header__user-name.font-700 {{ username }}
+      .header__user-dropdown-arrow
+    transition(name="fade")
+      .header__user-dropdown(v-show="dropdownOpen")
+        ul.header__user-dropdown-list
+          li.header__user-dropdown-listitem
+            base-button.header__user-dropdown-link(
+              tag="router-link"
+              :to="{ name: 'dashboard' }"
+              @click="hideDropdown"
+              icon="home"
+              variant="link"
+              size="small") Home
+          li.header__user-dropdown-listitem
+            base-button.header__user-dropdown-link(
+              tag="router-link"
+              :to="{ name: 'profile' }"
+              @click="hideDropdown"
+              icon="account_circle"
+              variant="link"
+              size="small") My Profile
+          li.header__user-dropdown-listitem
+            base-button.header__user-dropdown-link(icon="group" variant="link" size="small" disabled) Group Chat
+          li.header__user-dropdown-listitem.header__user-dropdown-listitem--line
+          li.header__user-dropdown-listitem
+            base-button.header__user-dropdown-link(@click="logoutUser" icon="logout" color="danger" variant="link" size="small") Logout
+  slot
 </template>
 
 <script>
--- src/views/Unsplash.vue
+++ src/views/Unsplash.vue
@@ -4,6 +4,13 @@
     template(#logo)
       router-link(:to="{ name: 'unsplash' }")
         svg-logo-my-unsplash
+    template(#default)
+      .header__search
+        .form.form--search
+          vee-form.form__form(:validation-schema="schema" @submit="submit")
+            .form__row
+              base-input(name="search" icon="search" placeholder="Search by name")
+      base-button.header__add-photo.font-700(color="primary") Add photo
   main
     p Unsplash
   base-footer
@@ -13,6 +20,8 @@
 import useBodyClass from '@/hooks/useBodyClass';
 
 import SvgLogoMyUnsplash from '@/components/svg/LogoMyUnsplash.vue';
+
+import '@/assets/scss/modules/unsplash/main.scss';
 
 export default {
   name: 'Unsplash',
