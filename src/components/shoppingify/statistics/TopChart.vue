9f8b560a23e1a8abf75b86cc4cee79c279c6b0a9
kczapko <kczapko@gmail.com>
2022-01-02 10:15:32 +0100

    animated top charts

--- src/components/shoppingify/statistics/TopChart.vue
+++ src/components/shoppingify/statistics/TopChart.vue
@@ -2,17 +2,17 @@
 section.top-chart
   h2.top-chart__title {{ title }}
   ul.top-chart__stats
-    li.top-chart__stat(v-for="item in items")
-      .top-chart__info
-        p.top-chart__name {{ item.name }}
-        span.top-chart__share {{ Math.round((item.count / total) * 100) }}%
-      .top-chart__chart
-        progress.top-chart__progress(:value="item.count" :max="total")
+    top-chart-bar(v-for="(item, i) in items" :name="item.name" :value="item.count" :total="total" :delay="i * 0.2")
 </template>
 
 <script>
+import TopChartBar from '@/components/shoppingify/statistics/TopChartBar.vue';
+
 export default {
   name: 'TopChart',
+  components: {
+    TopChartBar,
+  },
   props: {
     title: {
       type: String,
---
+++ src/components/shoppingify/statistics/TopChartBar.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+li.top-chart__stat
+  .top-chart__info
+    p.top-chart__name {{ name }}
+    span.top-chart__share {{ Math.round((finalValue / total) * 100) }}%
+  .top-chart__chart
+    progress.top-chart__progress(:value="finalValue" :max="total")
+</template>
+
+<script>
+import { gsap } from 'gsap';
+
+export default {
+  name: 'TopChartBar',
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    delay: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      finalValue: 0,
+    };
+  },
+  mounted() {
+    gsap.to(this.$data, { finalValue: this.value, duration: 0.8, delay: this.delay });
+  },
+};
+</script>
